7bf8bc0b182d3a02bc0d30b5b3eb3cd1192b4b4a
Eduardo Lozano <eduardoellozi2023@gmail.com>
2024-07-04 21:36:57 -0300

    adicionando testes unitarios para o servico de criptografia

---
+++ Testes/TesteBase.cs
@@ -0,0 +1,22 @@
+﻿using Aplicacao;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Testes
+{
+    public class TesteBase : IDisposable
+    {
+        internal readonly ServiceProvider _serviceProvider;
+
+        public TesteBase() 
+        {
+            var services = new ServiceCollection();
+            ModuloDeInjecaoDaAplicacao.RegistrarServicos(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+            _serviceProvider.Dispose();
+        }
+    }
+}
---
+++ Testes/Testes/teste_servico_criptografia.cs
@@ -0,0 +1,45 @@
+﻿using Aplicacao.Servicos;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography;
+
+namespace Testes.TesteServicoCriptografia
+{
+    public class teste_servico_criptografia : TesteBase
+    {
+        private readonly CriptografiaService _servicoCriptografia;
+        public teste_servico_criptografia()
+        {
+            _servicoCriptografia = _serviceProvider.GetService<CriptografiaService>()
+                ?? throw new Exception("CriptografiaService não foi encontrado.");
+        }
+
+        [Fact]
+        public void AoCriptografarSenhaDeveRetornarArrayDeBytesNaoNulo()
+        {
+            var senha = "kldjhfdsf124@!";
+            var chave = new byte[32];
+            var iv = new byte[16];
+            RandomNumberGenerator.Fill(chave);
+            RandomNumberGenerator.Fill(iv);
+
+            var senhaCriptografada = _servicoCriptografia.Criptografar(senha, chave, iv);
+            Assert.IsType<byte[]>(senhaCriptografada);
+            Assert.NotNull(senhaCriptografada);
+        }
+
+        [Fact]
+        public void AoDescriptografarSenhaDeveBaterComSenhaOriginal()
+        {
+            var senha = "kldjhfdsf124@!";
+            var chave = new byte[32];
+            var iv = new byte[16];
+            RandomNumberGenerator.Fill(chave);
+            RandomNumberGenerator.Fill(iv);
+
+            var senhaCriptografada = _servicoCriptografia.Criptografar(senha, chave, iv);
+            var senhaDescriptograda = _servicoCriptografia.Descriptografar(senhaCriptografada, chave, iv);
+            Assert.Equal(senha, senhaDescriptograda);
+        }
+
+    }
+}
